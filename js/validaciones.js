48af125dff12f5bda9f6b3cd8f38ac723baad14c
FabianDUOC <fa.marileo@duocuc.cl>
2022-05-01 21:04:03 -0400

    3.3
    Actualización de validaciones:
     - Editar cuentas
     - Editar y productos
    
    Actualización en estructura de formulario de registro y editar cuenta.

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //FOrmulario INICIO SESIÓN
-    $("#iniciar").submit(function(e){
+    $("#iniciar").submit(function (e) {
         e.preventDefault();
     })
 
     // FORMULARIO RECUPERAR CONTRASEÑA
-    $("#formRecupClave").submit(function(e){
+    $("#formRecupClave").submit(function (e) {
         e.preventDefault();
         var correo1 = $("#correo1").val();
         var correo2 = $("#correo2").val();
@@ -13,23 +13,23 @@ $(document).ready(function(){
         let msj = "";
         let entrar = false;
 
-        if(correo1!=correo2){
-            msj+="Los correos ingresados deben ser iguales <br>";
+        if (correo1 != correo2) {
+            msj += "Los correos ingresados deben ser iguales <br>";
             entrar = true;
         }
 
-        if(entrar){
+        if (entrar) {
             $("#mensajes").html(msj);
         }
-        else{
-            $("#mensajes").css({'color':'blue'});
+        else {
+            $("#mensajes").css({ 'color': 'blue' });
             $("#mensajes").html("Correo de recuperación enviado");
             $("#formRecupClave")[0].reset();
         }
     })
 
     // FORMULARIO CAMBIAR CONTRASEÑA
-    $("#cambiarClave").submit(function(e){
+    $("#cambiarClave").submit(function (e) {
         e.preventDefault();
         var clave = $("#claveActual").val();
         var claveNu1 = $("#claveNueva1").val();
@@ -38,32 +38,32 @@ $(document).ready(function(){
         let msj = "";
         let entrar = false;
 
-        if(claveNu1.trim().length < 6){
-            msj+="La contraseña nueva de tener al menos 6 caracteres <br>";
+        if (claveNu1.trim().length < 6) {
+            msj += "La contraseña nueva de tener al menos 6 caracteres <br>";
             entrar = true;
         }
-        if(claveNu1==clave){
-            msj+="Las contraseñas nuevas deben ser distinta a la contraseña antigua <br>"
+        if (claveNu1 == clave) {
+            msj += "Las contraseñas nuevas deben ser distinta a la contraseña antigua <br>"
             entrar = true;
         }
-        if(claveNu1!=claveNu2){
-            msj+="Las contraseñas nuevas deben ser iguales <br>"
+        if (claveNu1 != claveNu2) {
+            msj += "Las contraseñas nuevas deben ser iguales <br>"
             entrar = true;
         }
 
 
-        if(entrar){
+        if (entrar) {
             $("#mensajes").html(msj);
         }
-        else{
-            $("#mensajes").css({'color':'blue'});
+        else {
+            $("#mensajes").css({ 'color': 'blue' });
             $("#mensajes").html("Contraseña cambiada exitósamente");
             $("#cambiarClave")[0].reset();
         }
     })
 
     // FORMNULARIO CONTÁCTANOS
-    $("#formContanto").submit(function(e){
+    $("#formContanto").submit(function (e) {
         e.preventDefault();
 
         var nom = $("#nombre").val();
@@ -71,27 +71,29 @@ $(document).ready(function(){
         let msj = "";
         let entrar = false;
 
-        if(nom.trim().length < 4){
-            msj+="EL nombre de tener al menos 4 caracteres <br>";
+        if (nom.trim().length < 4) {
+            msj += "EL nombre de tener al menos 4 caracteres <br>";
             entrar = true;
         }
 
-        if(entrar){
+        if (entrar) {
             $("#mensajes1").html(msj);
         }
-        else{
+        else {
             $("#mensajes1").html("");
-            $("#mensajes2").css({'color':'blue'});
+            $("#mensajes2").css({ 'color': 'blue' });
             $("#mensajes2").html("Mensaje enviado exitosamente");
             $("#formContanto")[0].reset();
         }
     })
 
     // FORMULARIO REGISTRO DE CUENTA
-    $("#formRegistro").submit(function(e){
+    $("#formRegistro").submit(function (e) {
         e.preventDefault();
 
-        var nom = $("#nomCompleto").val();
+        var nom = $("#nombre").val();
+        var apellidoPa = $("#apellidoPa").val();
+        var apellidoMa = $("#apellidoMa").val();
         var email = $("#correoRegistro").val();
         var direcc = $("#direccion").val();
         var region = $("#region").val();
@@ -104,61 +106,136 @@ $(document).ready(function(){
         let msjClaves = "";
 
         $("#msjNom").html("");
+        $("#msjApellidoPa").html("");
+        $("#msjApellidoMa").html("");
         $("#msjDireccion").html("");
         $("#msjRegion").html("");
         $("#msjComuna").html("");
         $("#msjTel").html("");
         $("#msjClave").html("");
 
-        if(nom.trim().length < 4){
-            $("#msjNom").html("EL nombre debe tener al menos 4 caracteres");
+        if (nom.trim().length < 4 || nom.trim().length > 10) {
+            $("#msjNom").html("EL nombre no tiene la longitud válida");
             entrar = true;
         }
 
-        if(direcc.trim().length < 4){
+        if (apellidoPa.trim().length < 4 || apellidoPa.trim().length > 10) {
+            $("#msjApellidoPa").html("EL apellido no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (apellidoMa.trim().length < 4 || apellidoMa.trim().length > 10) {
+            $("#msjApellidoMa").html("EL apellido no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (direcc.trim().length < 4) {
             $("#msjDireccion").html("La dirección debe tener al menos 4 caracteres");
             entrar = true;
         }
 
-        if(region=="Escoge tu Región"){
+        if (region == "Escoge tu Región") {
             $("#msjRegion").html("Seleccionar región")
             entrar = true;
         }
 
-        if(numTel.trim().length != 9){
+        if (numTel.trim().length != 9) {
             $("#msjTel").html("Numero de telefono de 9 dígitos");
             entrar = true;
         }
 
-        if(comuna=="Escoge tu Comuna"){
+        if (comuna == "Escoge tu Comuna") {
             $("#msjComuna").html("Seleccionar comuna")
             entrar = true;
         }
 
-        if(clave1.trim().length < 6){
-            msjClaves+="La contraseña de tener al menos 6 caracteres <br>";
+        if (clave1.trim().length < 6) {
+            msjClaves += "La contraseña de tener al menos 6 caracteres <br>";
             entrar = true;
         }
 
-        if(clave1!=clave2){
-            msjClaves+="Las contraseñas deben ser iguales";
+        if (clave1 != clave2) {
+            msjClaves += "Las contraseñas deben ser iguales";
             entrar = true;
         }
 
-        if(entrar){
+        if (entrar) {
             $("#msjClave").html(msjClaves);
         }
-        else{
-            $("#msj").css({'color':'blue'});
+        else {
+            $("#msj").css({ 'color': 'blue' });
             $("#msj").html("Cuenta creada exitosamente");
             $("#formRegistro")[0].reset();
         }
-        
-        
     })
 
-    // FORMULARIO EDITAR PRODUCTO
-    $("#agregarP").submit(function(e){
+    // FORMULARIO EDITAR CUENTA
+    $("#editCuenta").submit(function (e) {
+        e.preventDefault();
+
+        var nom = $("#nombre").val();
+        var apellidoPa = $("#apellidoPa").val();
+        var apellidoMa = $("#apellidoMa").val();
+        var direcc = $("#direccion").val();
+        var region = $("#region").val();
+        var comuna = $("#comuna").val();
+        var numTel = $("#telefono").val();
+
+        let entrar = false;
+
+        $("#msjNom").html("");
+        $("#msjApellidoPa").html("");
+        $("#msjApellidoMa").html("");
+        $("#msjDireccion").html("");
+        $("#msjRegion").html("");
+        $("#msjComuna").html("");
+        $("#msjTel").html("");
+
+        if (nom.trim().length < 4 || nom.trim().length > 10) {
+            $("#msjNom").html("EL nombre no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (apellidoPa.trim().length < 4 || apellidoPa.trim().length > 10) {
+            $("#msjApellidoPa").html("EL apellido no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (apellidoMa.trim().length < 4 || apellidoMa.trim().length > 10) {
+            $("#msjApellidoMa").html("EL apellido no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (direcc.trim().length < 4) {
+            $("#msjDireccion").html("La dirección debe tener al menos 4 caracteres");
+            entrar = true;
+        }
+
+        if (region == "Escoge tu Región") {
+            $("#msjRegion").html("Seleccionar región")
+            entrar = true;
+        }
+
+        if (numTel.trim().length != 9) {
+            $("#msjTel").html("Numero de telefono de 9 dígitos");
+            entrar = true;
+        }
+
+        if (comuna == "Escoge tu Comuna") {
+            $("#msjComuna").html("Seleccionar comuna")
+            entrar = true;
+        }
+
+        if (entrar) {
+        }
+        else {
+            $("#msj").css({ 'color': 'blue' });
+            $("#msj").html("Se han guardado los cambios");
+        }
+    })
+
+    // FORMULARIO AGREGAR PRODUCTO
+    $("#agregarP").submit(function (e) {
         e.preventDefault();
 
         var nombre = $("#nombreProducto").val();
@@ -173,65 +250,115 @@ $(document).ready(function(){
         $("#msjCorta").html("");
         $("#msjDetallada").html("");
         $("#msjCantidad").html("");
-        $("#msjCategoria").html("");
+        $("#msjCategorias").html("");
 
         let entrar = false;
 
-        if(nombre.trim().length < 4 || nombre.trim().length > 15){
+        if (nombre.trim().length < 4 || nombre.trim().length > 15) {
             $("#msjNombre").html("EL nombre de producto no tiene la longitud válida");
             entrar = true;
         }
 
-        if(precio == 0){
+        if (precio == 0) {
             $("#msjPrecio").html("Precio debe ser mayor a 0")
             entrar = true;
         }
 
-        if(desCorta.trim().length < 4|| desCorta.trim().length >20){
+        if (desCorta.trim().length < 4 || desCorta.trim().length > 20) {
             $("#msjCorta").html("La descripción no tiene la longitud válida");
             entrar = true;
         }
 
-        if(desLarga.trim().length < 10|| desLarga.trim().length >200){
+        if (desLarga.trim().length < 10 || desLarga.trim().length > 200) {
             $("#msjDetallada").html("La descripción no tiene la longitud válida");
             entrar = true;
         }
 
-        if(cantidad == 0){
+        if (cantidad == 0) {
             $("#msjCantidad").html("Cantidad debe ser mayor a 0")
             entrar = true;
         }
 
-        if(categoria=="Escoge La Categoría"){
-            $("#msjCategoria").html("Seleccionar una categoría")
+        if (categoria == "Categoría") {
+            $("#msjCategorias").html("Debe seleccionar una categoría")
             entrar = true;
         }
 
-        if(entrar){     
+        if (entrar) {
         }
-        else{
-            $("#msj").css({'color':'blue'});
+        else {
+            $("#msj").css({ 'color': 'blue' });
             $("#msj").html("Producto Agregado");
             $("#agregarP")[0].reset();
         }
-
-
-
     })
+
+    // FORMULARIO EDITAR PRODUCTO
+    $("#editarP").submit(function (e) {
+        e.preventDefault();
+
+        var nombre = $("#nombreProducto").val();
+        var precio = $("#precioP").val();
+        var desCorta = $("#descripcionCorta").val();
+        var desLarga = $("#descripcionDetallada").val();
+        var cantidad = $("#cantidad").val();
+        var categoria = $("#categoria").val();
+
+        $("#msjNombre").html("");
+        $("#msjPrecio").html("");
+        $("#msjCorta").html("");
+        $("#msjDetallada").html("");
+        $("#msjCantidad").html("");
+        $("#msjCategorias").html("");
+
+        let entrar = false;
+
+        if (nombre.trim().length < 4 || nombre.trim().length > 15) {
+            $("#msjNombre").html("EL nombre de producto no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (precio == 0) {
+            $("#msjPrecio").html("Precio debe ser mayor a 0")
+            entrar = true;
+        }
+
+        if (desCorta.trim().length < 4 || desCorta.trim().length > 20) {
+            $("#msjCorta").html("La descripción no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (desLarga.trim().length < 10 || desLarga.trim().length > 200) {
+            $("#msjDetallada").html("La descripción no tiene la longitud válida");
+            entrar = true;
+        }
+
+        if (cantidad == 0) {
+            $("#msjCantidad").html("Cantidad debe ser mayor a 0")
+            entrar = true;
+        }
+
+        if (categoria == "Categoría") {
+            $("#msjCategorias").html("Debe seleccionar una categoría")
+            entrar = true;
+        }
+
+        if (entrar) {
+        }
+        else {
+            $("#msj").css({ 'color': 'blue' });
+            $("#msj").html("Información de producto modificada");
+        }
+    })
+
 
     // FORMULARIO MENSAJES ADMIN
-    $("#formMensaje").submit(function(e){
+    $("#formMensaje").submit(function (e) {
         e.preventDefault();
-        $("#mensajes").css({'color':'blue'});
-        $("#mensajes").html("Mensaje Enviado Satisfactoriamente")
+        $("#mensajes").css({ 'color': 'blue' });
+        $("#mensajes").html("Mensaje enviado satisfactoriamente")
+        $("#formMensaje")[0].reset();
     })
-
-
-
-
-
-
-
 
 
 })
